3161fbfd77b19fe207e0fca699298eb20750656e
Indar Basto <indar.basto@gmail.com>
2023-04-11 13:33:34 +0300

    feat: add static server

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import jsonServer from "json-server";
 import cors from "cors";
-import places from "./places/places";
+import places from "./db/places/places";
+
+const db = {
+  places,
+};
 
 const server = jsonServer.create();
-const router = jsonServer.router({ places });
-const middleWires = jsonServer.defaults();
+const router = jsonServer.router(db);
+const middleWares = jsonServer.defaults({ static: __dirname + "/public" });
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 server.use(cors());
-server.use(middleWires);
 server.use(router);
+server.use(middleWares);
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
